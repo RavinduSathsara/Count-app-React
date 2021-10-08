1540575fa2d0d08f222822f8a3714c1089d33775
RavinduSathsara <rsathsara330@gmail.com>
2021-10-08 21:32:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, ButtonGroup } from "react-bootstrap";
+import { useState } from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function goRight() {
+    setCount(count + 1);
+  }
+
+  function goLeft() {
+    setCount(count - 1);
+  }
+
+  function middle() {
+    setCount(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Count App</h1>
+      <span>{count}</span>
+      <hr />
+      <ButtonGroup aria-label="Basic example">
+        <Button variant="secondary" onClick={goLeft}>
+          Left
+        </Button>
+        <Button variant="secondary" onClick={middle}>
+          Middle
+        </Button>
+        <Button variant="secondary" onClick={goRight}>
+          Right
+        </Button>
+      </ButtonGroup>
     </div>
   );
 }
